e2e6bbbd16bad4226cb5f353ecd4d0edbff4fe2d
João Moura <joao.moura@b-i.com>
2020-06-26 23:32:44 +0100

    Added hourly, current and oneCall API's

--- mod.ts
+++ mod.ts
@@ -1,167 +1,16 @@
+import { currentWrapper } from './currentWrapper.ts'
+import { hourlyWrapper } from './hourlyWrapper.ts'
+import { oneCallWrapper } from './oneCallWrapper.ts'
+
 const OPENWEATHER_URL = 'https://api.openweathermap.org/data/2.5'
 
-type serializableObject = {[key: string]: string | number | undefined}
-
-type currentWeatherResponse = {
-  coord: {
-    lon: number,
-    lat: number
-  }
-  weather: {
-    id: number,
-    main: string,
-    description: string,
-    icon: string
-  }[]
-  base: string,
-  main: {
-    temp: number,
-    feels_like: number,
-    pressure: number,
-    humidity: number,
-    temp_min: number,
-    temp_max: number,
-    sea_level: number,
-    grnd_level: number,
-  },
-  visibility: number,
-  wind: {
-    speed: number,
-    deg: number,
-    gust: number,
-  },
-  clouds: {
-    all: number,
-  }
-  rain: {
-    '1h': number,
-    '3h': number,
-  },
-  snow: {
-    '1h': number,
-    '3h': number,
-  }
-  dt: number
-  sys: {
-    type: number,
-    id: number,
-    message: number,
-    country: string,
-    sunrise: number,
-    sunset: number,
-  }
-  timezone: number,
-  id: number,
-  name: string,
-  cod: number,
-}
-
-class currentWrapper {
-  private endpoint = '/weather?'
-  private requester: OpenWeather["request"]
-  constructor(requester: OpenWeather["request"]) {
-    this.requester = requester
-  }
-
-  private request(endpoint: string, obj: serializableObject, lang?: string) {
-    return this.requester(endpoint, obj, lang) as Promise<currentWeatherResponse>
-  }
-
-  byCityName(name: string, { state, country , lang }: { state?: string, country?: string, lang?: string }) {
-    return this.request(
-      this.endpoint,
-      {
-        q: `${name}${state ? ',' + state : ''}${country ? ',' + country : ''}`
-      },
-      lang
-    )
-  }
-
-  byCityId(id: number | number[], lang?: string) {
-    const isArray = Array.isArray(id)
-    return this.request(
-      isArray ? '/group?' : this.endpoint,
-      {
-        id: `${Array.isArray(id) ? id.join(',') : id}`
-      },
-      lang
-    )
-  }
-
-  byGeographic(lat: string, lon: string, lang?: string) {
-    return this.request(
-      this.endpoint,
-      {
-        lat,
-        lon
-      },
-      lang
-    )
-  }
-
-  byZipId(zip: string, { country, lang}:{ country?: string, lang?: string }) {
-    return this.request(
-      this.endpoint,
-      {
-        zip: `${zip}${country ? ',' + country : ''}`
-      },
-      lang
-    )
-  }
-
-  byRectangle(
-    lonLeft: number,
-    latBottom: number,
-    lonRight: number,
-    latTop: number,
-    zoom: number,
-    {
-      cluster,
-      lang
-    }: {
-      cluster?: boolean,
-      lang?: string
-    }
-  ) {
-    return this.request(
-      '/box/city?',
-      {
-        bbox: `${lonLeft},${latBottom},${lonRight},${latTop},${zoom}`,
-        cluster: `${(cluster !== undefined && cluster ? 'yes' : 'no') || ''}`,
-      },
-      lang
-    )
-  }
-
-  byCircle(
-    lat: number,
-    lon: number,
-    {
-      cluster,
-      cnt,
-      lang
-    }: {
-      cluster?: boolean,
-      cnt?: number,
-      lang?: string
-    }
-  ) {
-    return this.request(
-      '/find?',
-      {
-        lat,
-        lon,
-        cluster: `${(cluster !== undefined && cluster ? 'yes' : 'no') || ''}`,
-        cnt,
-      },
-      lang
-    )
-  }
-}
+export type serializableObject = {[key: string]: string | number | undefined}
 
 export class OpenWeather {
   private apiKey: string
   public current: currentWrapper
+  public hourly: hourlyWrapper
+  public oneCall: oneCallWrapper
   private lang = 'en'
   constructor(apiKey: string, lang?: string) {
     this.apiKey = apiKey
@@ -169,6 +18,8 @@ export class OpenWeather {
       this.lang = lang
     }
     this.current = new currentWrapper(this.request)
+    this.hourly = new hourlyWrapper(this.request)
+    this.oneCall = new oneCallWrapper(this.request)
   }
 
   public setApiKey(key: string) {
@@ -185,7 +36,7 @@ export class OpenWeather {
 
   private async request(endpoint: string, obj: serializableObject, lang?: string) {
     try {
-      const res = await fetch(`${OPENWEATHER_URL}${endpoint}${this.serializeQueryString(obj)}&appid=${this.apiKey}${lang ? '&lang=' + lang : ''}`)
+      const res = await fetch(`${OPENWEATHER_URL}${endpoint}${this.serializeQueryString(obj)}&appid=${this.apiKey}&lang=${lang || this.lang}`)
       return res.json()
     } catch(e) {
       throw e
